590fc6fddcdf13ab1d1bd81fa1ee81a40a932cf5
NineBird <no-reply@ninebird.io>
2023-06-17 21:50:20 +0800

    Add ssg builder

---
+++ .layouts/gatsby-config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const pathPrefix = "/system-design-interview-book-club/";
+const siteMetadata = {
+  title: "讀書會",
+  shortName: "讀書會",
+  description: "就是讀書會",
+  siteUrl: "https://warren30815.github.io/",
+};
+module.exports = {
+  siteMetadata,
+  pathPrefix,
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/..`,
+        ignore: [`**/\.*/**/*`],
+      },
+    },
+    {
+      resolve: "gatsby-theme-primer-wiki",
+      options: {
+        nav: [
+          {
+            title: "Github",
+            url: "https://github.com/warren30815/system-design-interview-book-club",
+          },
+        ],
+        editUrl:
+          "https://github.com/warren30815/system-design-interview-book-club/tree/main/",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortName,
+        start_url: pathPrefix,
+        background_color: `#f7f0eb`,
+        display: `standalone`,
+        icon: path.resolve(__dirname, "./static/logo.png"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteMetadata.siteUrl,
+        sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [],
+      },
+    },
+  ],
+};
