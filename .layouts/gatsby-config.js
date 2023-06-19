f73af4425b09576478c305f4c5380e15931bc8ef
jordan <qaz7821819@gmail.com>
2023-06-19 22:47:31 +0800

    fix: reset gatsby config

--- .layouts/gatsby-config.js
+++ .layouts/gatsby-config.js
@@ -17,7 +17,8 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "content",
-        path: `${__dirname}/../content`,
+        path: `${__dirname}/..`,
+        ignore: [`**/\.*/**/*`],
       },
     },
     {
